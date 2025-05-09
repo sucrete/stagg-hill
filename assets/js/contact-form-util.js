e81c5a97408bbe88662dc30d5405ce0fcef0c1fa
Will Andree <williamandree@gmail.com>
2025-05-09 10:21:47 -0500

    Updated contact form util.

--- assets/js/contact-form-util.js
+++ assets/js/contact-form-util.js
@@ -1,29 +1,37 @@
 (function () {
-  emailjs.init('bgPMvjgJ9C-HJ3z9j');
+  emailjs.init("bgPMvjgJ9C-HJ3z9j");
 })();
 
-const btn = document.getElementById('button');
-const firstName = document.getElementById('name');
-const lastName = document.getElementById('last');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const message = document.getElementById('message');
+const btn = document.getElementById("button");
+const firstName = document.getElementById("name");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
 
-document.getElementById('contact-form')
-.addEventListener('submit', function(event) {
-  event.preventDefault();
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  btn.value = 'Sending...';
+    btn.value = "Sending...";
 
-  const serviceID = 'default_service';
-  const templateID = 'template_gs2wivh';
+    const serviceID = "default_service";
+    const templateID = "template_gs2wivh";
 
-  emailjs.sendForm(serviceID, templateID, this)
-   .then(() => {
-     btn.value = 'Send Email';
-     alert('Message Sent');
-   }, (err) => {
-     btn.value = 'Send Email';
-     alert(JSON.stringify(err));
-   });
-});
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Submit";
+        firstName.value = "";
+        lastName.value = "";
+        email.value = "";
+        phone.value = "";
+        message.value = "";
+        alert("Message Sent");
+      },
+      (err) => {
+        btn.value = "Submit";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
