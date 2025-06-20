9b12a6d346ce2f0833749109fde67b46dc24d5ce
Will Andree <williamandree@gmail.com>
2025-06-20 14:09:41 -0500

    Updating weather widget.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,7 +1,7 @@
 //~ Standard Variables
 // API URL
 const apiUrl =
-  "https://api.pirateweather.net/forecast/ntC8U5xrrc5kn10XQebmEvssDHcleOe5/39.152,-96.636?exclude=minutely,hourly,alerts,flags";
+  "https://api.pirateweather.net/forecast/EqRw5datS5zL99ze3FwQ8Q7PJEtJAC0i/39.152,-96.636?exclude=minutely,hourly,alerts,flags";
 
 // Days of the week
 const days = [
